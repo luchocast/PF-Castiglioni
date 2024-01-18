cff109860f2a9061151afc1b9df5b3341599adcd
luciano <lucianocasti27@gmail.com>
2024-01-18 18:11:09 -0300

    Agrego Array de Objetos con cursos y consultorias. Agrego metodos para los array y outputs

--- js/main.js
+++ js/main.js
@@ -45,3 +45,59 @@ let edad = validarEdad();
 
 console.log("El nombre ingresado es: ", nombre);
 console.log("La eda ingresada es: ", edad);
+
+
+
+
+
+
+
+// Creo lista de cursos a ofrecer (Array de objetos)
+
+const cursos = [
+    {nombre: 'Productividad Maestra', precio: '20.000', horas: '12hs', modulos: '8'},
+    {nombre: 'Disciplina de Monje', precio: '30.000', horas: '10hs', modulos: '9'},
+    {nombre: 'Lectura Proactiva', precio: '10.000', horas: '6hs', modulos: '6'},
+    {nombre: 'Habilidades sociales', precio: '13.000', horas: '7hs', modulos: '4'}
+];
+
+// En caso de querer agregar cursos en un futuro (metodo push)
+
+//cursos.push()
+
+// Creo otro Array Objects para consultorias/mentorias
+const consultorias = [
+    {nombre: 'Protocolo Redes Sociales', precio: '200.000', duracion: '1 mes', llamadas: '1/week'},
+    {nombre: 'Protocolo Marca personal', precio: '100.000', duracion: '1 mes', llamadas: '1/week'},
+    {nombre: 'Protocolo Fisico Estetico', precio: '300.000', duracion: '2 mes', llamadas: '1/week'}
+];
+
+// En caso de querer eliminar consultorias en un futuro (metodo pop o splice)
+
+//consultorias.splice()
+
+
+
+
+// Itero sobre el Array de cursos
+// Creo Variable que guarde la HOF (no hace falta return, ya que no hay {})
+
+const cursoEncontrado = cursos.find((item) => item.nombre === 'Productividad Maestra');
+
+// Output
+if (cursoEncontrado) {
+    console.table(cursoEncontrado);
+} else {
+    console.log("Curso no encontrado");
+}
+
+
+
+// Itero sobre el Array de consultoria
+// Creo Variable que guarde la HOF
+
+const consultoriaEncontrada= consultorias.some((item) => item.nombre==='Redes Sociales');
+
+
+// Output
+console.log("Consultoría 'Redes Sociales' encontrada:", consultoriaEncontrada);
